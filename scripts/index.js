9f82b8bf791cf72f2796d079e8dec5f4e9caa07d
Tasdeed <ayonaziz@gmail.com>
2023-09-28 15:41:24 -0400

    btn functionality added

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the button and menu elements
+  var dropdownButton = document.getElementById("dropdownButton");
+  var dropdownMenuItems = document.querySelectorAll(".dropdown-item");
+
+  // Add a click event listener to each dropdown menu item
+  dropdownMenuItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      // Update the button text with the selected item's data-value
+      dropdownButton.innerText = this.getAttribute("data-value");
+    });
+  });
+});
+
+const loginBtn = () => {
+  window.location.href = "../pages/login.html";
+};
+
+const signUpBtn = () => {
+  window.location.href = "../pages/signup.html";
+};
